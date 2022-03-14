b560a59f8bd259857da2c763129ce142cb121e01
Arka <arkadatta2002@gmail.com>
2022-03-15 00:01:46 +0530

    Part3

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+// connect DB 
+const dbUrl = 'mongodb+srv://book:eduBl2eiLy3FqlFl@cluster0.ojgdo.mongodb.net/book-keeping-app'  // change test to the name of our app 
+mongoose.connect(dbUrl,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}).then(() => {
+    console.log('Db Connection established')
+}).catch(err => {
+    console.error(err)
+})
 
 const PORT = process.env.PORT || 3010;    // if deployed in heroku , otherwise on port 3010
 
@@ -36,3 +49,7 @@ app.get('/api/users',(req,res) => {
 app.listen(PORT,()=> {
     console.log(`Server is up and running ${PORT}`)
 })
+
+
+// book
+// eduBl2eiLy3FqlFl
