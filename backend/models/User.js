6f6c6ce70ce4a32534ddb85bceefc9a150c5f16b
Arka <arkadatta2002@gmail.com>
2022-03-15 07:25:38 +0530

    bcrypt use

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcryptjs')
 // Schema 
 
 const UserSchema = new mongoose.Schema({
@@ -16,6 +16,13 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     }
+})
+
+
+UserSchema.pre('save',async function (next) {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 const User = mongoose.model('User',UserSchema);
